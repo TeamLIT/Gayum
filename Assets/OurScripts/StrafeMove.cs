02e81a4ee09325fe689788ff7758f8011c5499d4
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2018-04-23 08:18:26 +0100

    Add files via upload
    strafe script for the player

---
+++ Assets/OurScripts/StrafeMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrafeMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * 0.1f);
+        }
+
+    }
+}
