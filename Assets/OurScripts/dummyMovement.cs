e2fcf79ba1b7282593019e5e79609cd582e4ae4c
mattwhelanUK <37943318+mattwhelanUK@users.noreply.github.com>
2018-04-25 15:58:08 +0100

    Dummy Edition
    dummies

---
+++ Assets/OurScripts/dummyMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummyMovement : MonoBehaviour
+{
+    bool hit = false;
+
+    float timer = 0.0f;
+
+    private Vector3 initialPos;
+    Vector3 newPos;
+    Vector3 returnMovement;
+
+	// Use this for initialization
+	void Start ()
+    {
+        initialPos = transform.localPosition;
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (transform.localPosition != initialPos)
+        {
+            newPos = transform.localPosition;
+            returnMovement = newPos - initialPos;
+            hit = true;   
+        }
+        else
+        {
+            hit = false;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (hit == true)
+        {
+            transform.position = initialPos;
+            hit = false;
+        }
+    }
+}
