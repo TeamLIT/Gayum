a8a6ee469668bdefdca092b8985a8d033e8cce9f
Dereyabi <Rsmxswordzz@yahoo.com>
2018-04-26 01:57:51 +0100

    Dummy
    dummy added, particles added, all weapons now hit crossheir, all weapon pickups work, weapons moved onto rack. Bugs: sword is pretty much entirely fucked, no idea how to fix, dummy spazzes out a bit but its still good enough. I have no idea how to put the object back onto the rack, i wish it was as easy as disabling/enabling the weapons but i dont know how to do that.

---
+++ Assets/OurScripts/dummyMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummyMovement : MonoBehaviour
+{
+    public ParticleSystem psystem;
+
+    bool hit = false;
+
+    private Vector3 initialPos;
+    private Vector3 initialRot;
+
+    private const float kEpsilon = 0.028f;
+
+    Vector3 newPos;
+    Vector3 returnMovement;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialRot = transform.eulerAngles;
+        initialPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.x > initialPos.x + kEpsilon || transform.localPosition.x < initialPos.x - kEpsilon || transform.localPosition.x > initialPos.x + kEpsilon || transform.localPosition.x < initialPos.x - kEpsilon)
+        {
+            newPos = transform.localPosition;
+            returnMovement = newPos - initialPos;
+            hit = true;
+        }
+        else
+        {
+            hit = false;
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (hit == true)
+        {
+            Debug.Log("It has been Hit");
+            transform.position = initialPos;
+            transform.Rotate(-transform.eulerAngles);
+            transform.eulerAngles = initialRot;
+            initialPos = transform.position;
+            hit = false;
+            psystem.Play();
+        }
+    }
+}
